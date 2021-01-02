3440a1f8387a5397a2e3a0e612345c8af06dfe35
Minjun <cksrb1616@gmail.com>
2021-01-02 16:58:34 +0100

    Couldn't find the way to wright the code in Tensorflow version 2. So, firstly finished code in tf1.

--- 02_using_saved_model_tf1.py
+++ 02_using_saved_model_tf1.py
@@ -1,25 +1,22 @@
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import numpy as np
 
-# 플레이스 홀더를 설정합니다.
+
 X = tf.placeholder(tf.float32, shape=[None, 4])
 Y = tf.placeholder(tf.float32, shape=[None, 1])
-
 W = tf.Variable(tf.random_normal([4, 1]), name="weight")
 b = tf.Variable(tf.random_normal([1]), name="bias")
 
-# 가설을 설정합니다.
 hypothesis = tf.matmul(X, W) + b
 
-# 저장된 모델을 불러오는 객체를 선언합니다.
 saver = tf.train.Saver()
 model = tf.global_variables_initializer()
 
-# 4가지 변수를 입력 받습니다.
-avg_temp = float(input('평균 온도: '))
-min_temp = float(input('최저 온도: '))
-max_temp = float(input('최고 온도: '))
-rain_fall = float(input('강수량: '))
+avg_temp = float(input('Average Temperature: '))
+min_temp = float(input('Minimum Temperature: '))
+max_temp = float(input('Maximum Temperature: '))
+rain_fall = float(input('Probability of Rain: '))
 
 with tf.Session() as sess:
     sess.run(model)
